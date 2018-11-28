6dd273658ebf7543c7fb036572374ed01a777e03
Jack Thibaut <jack.mk.thibaut@gmail.com>
2018-11-28 21:27:59 +0100

    feat: db up and running

---
+++ models/db.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const db = JSON.parse(fs.readFileSync('./data.json'))
+
+setInterval(() => {
+  fs.writeFile('./data.json', JSON.stringify(db), err => {
+    if (err) console.error(err)
+  })
+}, 60000)
+
+module.exports = db
